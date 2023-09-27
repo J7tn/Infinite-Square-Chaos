a7fba708a3ea2af1cf5a46abcad28f0b77037a9a
Flare <jntnnn4@gmail.com>
2023-09-26 23:22:43 -0700

    Update RandomDirection.cs

--- Assets/Scripts/RandomDirection.cs
+++ Assets/Scripts/RandomDirection.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RandomDirection : MonoBehaviour
 {
-    private Vector3 RandomVector(float min, float max)
+    public Vector3 RandomVector(float min, float max)
     {
         var x = Random.Range(min, max);
         var y = Random.Range(min, max);
@@ -14,7 +14,7 @@ public class RandomDirection : MonoBehaviour
     void Start()
     {
         var rb = GetComponent<Rigidbody>();
-        rb.velocity = RandomVector(-10f, 10f);
+        rb.velocity = RandomVector(-2f, 2f);
     }
 
     // Update is called once per frame
